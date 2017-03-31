d6a47ea19a1d162fc9561c19d6d91440af5db877
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-03-31 21:00:46 +0200

    Folders added

---
+++ Assets/Scripts/Randomizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomizer : MonoBehaviour {
+
+    public enum PlayerSelection
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
