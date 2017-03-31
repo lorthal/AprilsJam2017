0fb9f2b2726d7ce8b5c6d14d34fcb2a3a88fa478
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-03-31 22:21:15 +0200

    added first version of player1 and bonus randomizer

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speedVertical;
+    public float speedHorizontal;
+    public float maxVertical;
+    public float maxHorizontal;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 vel = new Vector3();
+
+        if (Input.GetKey(KeyCode.UpArrow) && vel.z <= maxHorizontal)
+        {
+            vel += transform.forward * speedHorizontal;
+        } 
+        if(Input.GetKey(KeyCode.DownArrow) && vel.z >= -maxHorizontal)
+        {
+            vel -= transform.forward * speedHorizontal;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow) && vel.x <= maxVertical)
+        {
+            vel -= transform.right * speedVertical;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && vel.x >= -maxVertical)
+        {
+            vel += transform.right * speedVertical;
+        }
+        rb.AddForce(vel);
+    }
+}
--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -5,14 +5,45 @@ using UnityEngine;
 public class Randomizer : MonoBehaviour {
 
     public enum PlayerSelection
+    {
+        Player1,
+        Player2,
+        Both
+    }
+
+    public PlayerSelection playerSelected { get; private set; }
+
+    private int tempRandom;
 
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(PlayerRandomizer());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    IEnumerator PlayerRandomizer()
+    {
+        while (true)
+        {
+            int random = 1;
+            do
+            {
+                tempRandom = random;
+                random = Random.Range(1, 4);
+                switch (random)
+                {
+                    case 1:
+                        playerSelected = PlayerSelection.Player1;
+                        break;
+                    case 2:
+                        playerSelected = PlayerSelection.Player2;
+                        break;
+                    case 3:
+                        playerSelected = PlayerSelection.Both;
+                        break;
+                }
+                Debug.Log(playerSelected);
+                yield return new WaitForSeconds(0.1f);
+            } while (tempRandom == random);
+        }
+    }
 }
