5c206de50601ef0ca2486b85571cee6faa541a6c
Rexim <rexim47@gmail.com>
2017-04-01 01:35:18 +0200

    Added Bonus Manager with slide and replace bonuses (scripts + prefabs). Added base for Game Controller.

---
+++ Assets/Scripts/Bonuses/BonusManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BonusManager : MonoBehaviour
+{
+    public BonusManager Instance { get; private set; }
+    public float timeBetweenBonusesChange = 3.0f;
+    private BonusBase currentBonus;
+    public GameObject[] bonusesPrefabs;
+    public Randomizer randomizer;
+
+	private void Start ()
+    {
+        Instance = this;
+        InvokeRepeating("RandomizeBonuses", timeBetweenBonusesChange, timeBetweenBonusesChange);
+	}
+
+    private void RandomizeBonuses()
+    {
+        int randomBonusId = Random.Range(0, bonusesPrefabs.Length);
+        GameObject currentBonusObj = Instantiate(bonusesPrefabs[randomBonusId], transform);
+        currentBonus = currentBonusObj.GetComponent<BonusBase>();
+        Debug.Log(currentBonus.gameObject.name);
+        switch (randomizer.playerSelected)
+        {
+            case Randomizer.PlayerSelection.Player1: currentBonus.ApplyBonus(GameController.Instance.Player1); break;
+            case Randomizer.PlayerSelection.Player2: currentBonus.ApplyBonus(GameController.Instance.Player2); break;
+            case Randomizer.PlayerSelection.Both:
+                {
+                    currentBonus.ApplyBonus(GameController.Instance.Player1);
+                    currentBonus.ApplyBonus(GameController.Instance.Player2);
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Bonuses/BonusSlide.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class BonusSlide : BonusBase
+{
+    private Rigidbody playerRigidbody;
+    private PlayerController playerController;
+    public float slideSpeed = 10.0f;
+    public int numberOfSpins = 2;
+    private float rotateSpeed;
+
+    private void Start()
+    {
+        rotateSpeed = 360.0f * numberOfSpins / duration;
+    }
+
+    public override void ApplyBonus(GameObject player)
+    {
+        this.player = player;
+        playerRigidbody = player.GetComponent<Rigidbody>();
+        playerController = player.GetComponent<PlayerController>();
+        StartCoroutine("SlideForSeconds");
+    }
+
+    private IEnumerator SlideForSeconds()
+    {
+        playerController.enabled = false;
+        playerRigidbody.velocity = slideSpeed * player.transform.forward;
+        //InvokeRepeating("RotateWhileSliding", Time.fixedDeltaTime, Time.fixedDeltaTime);
+        yield return new WaitForSeconds(duration);
+        //CancelInvoke("RotateWhileSliding");
+        playerRigidbody.velocity = Vector3.zero;
+        playerController.enabled = true;
+        Destroy(gameObject);
+    }
+
+    private void RotateWhileSliding()
+    {
+        player.transform.Rotate(transform.up, rotateSpeed * Time.fixedDeltaTime);
+    }
+}
