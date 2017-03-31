f11d5b1159751ace0f15b2783c9e46d9438e2ed5
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-03-31 23:08:19 +0200

    fix for player movement, added jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,29 +6,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
+    private bool isGrounded;
+
     public float speedVertical;
     public float speedHorizontal;
     public float maxVertical;
     public float maxHorizontal;
+    public float jumpForce;
+    public bool Player1;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        isGrounded = true;
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        if (Player1)
+            Player1Input();
+        else
+            Player2Input();
+    }
 
+    void Player1Input()
+    {
         Vector3 vel = new Vector3();
 
         if (Input.GetKey(KeyCode.UpArrow) && vel.z <= maxHorizontal)
         {
             vel += transform.forward * speedHorizontal;
-        } 
-        if(Input.GetKey(KeyCode.DownArrow) && vel.z >= -maxHorizontal)
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && vel.z >= -maxHorizontal)
         {
             vel -= transform.forward * speedHorizontal;
         }
-        if(Input.GetKey(KeyCode.LeftArrow) && vel.x <= maxVertical)
+        if (Input.GetKey(KeyCode.LeftArrow) && vel.x <= maxVertical)
         {
             vel -= transform.right * speedVertical;
         }
@@ -36,6 +48,54 @@ public class PlayerController : MonoBehaviour {
         {
             vel += transform.right * speedVertical;
         }
-        rb.AddForce(vel);
+        if (Input.GetKeyDown(KeyCode.RightControl) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+        if (vel.magnitude >= .75f)
+            rb.AddForce(vel);
+    }
+
+    void Player2Input()
+    {
+        Vector3 vel = new Vector3();
+
+        if (Input.GetKey(KeyCode.W) && vel.z <= maxHorizontal)
+        {
+            vel += transform.forward * speedHorizontal;
+        }
+        if (Input.GetKey(KeyCode.S) && vel.z >= -maxHorizontal)
+        {
+            vel -= transform.forward * speedHorizontal;
+        }
+        if (Input.GetKey(KeyCode.A) && vel.x <= maxVertical)
+        {
+            vel -= transform.right * speedVertical;
+        }
+        if (Input.GetKey(KeyCode.D) && vel.x >= -maxVertical)
+        {
+            vel += transform.right * speedVertical;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+        if (vel.magnitude >= .75f)
+            rb.AddForce(vel);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
