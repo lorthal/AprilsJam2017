baac252562eda62db4c32a656d2402945edbbef3
Rexim <rexim47@gmail.com>
2017-04-02 00:38:40 +0200

    Added bluescreen at the end (5 seconds after both players die).

---
+++ Assets/Scripts/Bluescreen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Bluescreen : MonoBehaviour
+{
+    public GameObject bluescreenBG;
+    public float delayBeforeBluescreen = 5.0f;
+    private bool bluescreenEnabled;
+
+    private void Start()
+    {
+        bluescreenEnabled = false;
+    }
+
+    private void Update()
+    {
+        if(GameController.Instance.Player1 == null && GameController.Instance.Player2 == null)
+        {
+            if (!bluescreenEnabled)
+                StartCoroutine("DisplayBluescreen");
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.R))
+                    SceneManager.LoadScene("main");
+            }
+        }
+    }
+
+    private IEnumerator DisplayBluescreen()
+    {
+        bluescreenEnabled = true;
+        yield return new WaitForSeconds(delayBeforeBluescreen);
+        bluescreenBG.SetActive(true);
+    }
+}
