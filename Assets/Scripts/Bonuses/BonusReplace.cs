3ee451b220c7c11ede114c33a8630b13c7f77508
Rexim <rexim47@gmail.com>
2017-04-01 20:58:48 +0200

    Bonus replace fix

--- Assets/Scripts/Bonuses/BonusReplace.cs
+++ Assets/Scripts/Bonuses/BonusReplace.cs
@@ -11,8 +11,16 @@ public class BonusReplace : BonusBase
     {
         Vector3 firstPlayerPosition = GameController.Instance.Player1.transform.position;
         Vector3 secondPlayerPosition = GameController.Instance.Player2.transform.position;
-
-        GameController.Instance.Player1.transform.position = secondPlayerPosition;
-        GameController.Instance.Player2.transform.position = firstPlayerPosition;
+        
+        if(firstPlayerPosition.z > secondPlayerPosition.z)
+        {
+            GameController.Instance.Player1.transform.position = secondPlayerPosition;
+            GameController.Instance.Player2.transform.position = firstPlayerPosition;
+        }
+        else
+        {
+            GameController.Instance.Player2.transform.position = firstPlayerPosition;
+            GameController.Instance.Player1.transform.position = secondPlayerPosition;
+        }
     }
 }
