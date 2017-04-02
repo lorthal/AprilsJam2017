45a0aa8bcc70b659924fa2fdf4981d7951fff424
Rexim <rexim47@gmail.com>
2017-04-02 03:40:07 +0200

    Added menu scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Button startButton;
+    public Button exitButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(OnStartButtonClick);
+        exitButton.onClick.AddListener(OnExitButtonClick);
+    }
+
+    private void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    private void OnExitButtonClick()
+    {
+        Application.Quit();
+    }
+}
