f377804243fd23c8326c9d3211c76cb1f757e90d
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-04-02 11:20:32 +0200

    added logo to menu

---
+++ Assets/LogoRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoRotator : MonoBehaviour {
+
+    public float rotationSpeed;
+
+    private RectTransform rTransorm;
+
+    private void Start()
+    {
+        rTransorm = GetComponent<RectTransform>();
+    }
+
+    void Update () {
+        rTransorm.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+	}
+}
